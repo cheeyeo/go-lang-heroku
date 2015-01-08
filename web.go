45d2e7b5a5c7c489b42797134619338066b9c4fb
Chee Yeo <ckyeo.1@gmail.com>
2015-01-08 23:39:24 +0000

    First

---
+++ web.go
@@ -0,0 +1,22 @@
+// PORT=5000 ./web
+
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "os"
+)
+
+func main(){
+  http.HandleFunc("/", hello)
+  fmt.Println("listening...")
+  err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+  if err != nil{
+    panic(err)
+  }
+}
+
+func hello(res http.ResponseWriter, req *http.Request){
+  fmt.Fprintln(res, "hello world")
+}
